11e96ff7c56655cb01572888a657ceef6baebbce
palaciosr <36349034+palaciosr@users.noreply.github.com>
2018-02-25 19:08:56 -0800

    Update edit_distance.cpp

--- dynami_progam#4/edit_distance/edit_distance.cpp
+++ dynami_progam#4/edit_distance/edit_distance.cpp
@@ -24,7 +24,7 @@ int edit_distance(const string str1, const string str2) // changed pass by refer
                     int x,y;
                     x= min (D[i-1][j]+1, D[i][j-1]+1);
                     y= min(x,D[i-1][j-1] + (str1[i-1] == str2[j-1] ? 0 : 1));
-					// need to look at waht ?0:1 mean has to do with if and while loops
+			
                     D[i][j]=y;
                 }
             }
