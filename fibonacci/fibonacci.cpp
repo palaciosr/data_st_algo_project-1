86e81cdb281b9327f6c8bc435e4c5ab0bd8a12fd
palaciosr <36349034+palaciosr@users.noreply.github.com>
2018-03-04 15:44:22 -0800

    Update fibonacci.cpp

--- fibonacci/fibonacci.cpp
+++ fibonacci/fibonacci.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 // The following code calls a naive algorithm for computing a Fibonacci number.
-//
+// instructions 
 // What to do:
 // 1. Compile the following code and run it on an input "40" to check that it is slow.
 //    You may also want to submit it to the grader to ensure that it gets the "time limit exceeded" message.
@@ -12,16 +12,11 @@ using namespace std;
 // 3. Remove the line that prints the result of the naive algorithm, comment the lines reading the input,
 //    uncomment the line with a call to test_solution, compile the program, and run it.
 //    This will ensure that your efficient algorithm returns the same as the naive one for small values of n.
-
-
-
-
-
 // 4. If test_solution() reveals a bug in your implementation, debug it, fix it, and repeat step 3.
 // 5. Remove the call to test_solution, uncomment the line with a call to fibonacci_fast (and the lines reading the input),
 //    and submit it to the grader.
 // Need  to have a "base case " of n=20 brute force way and with a much faster algo
-
+// Naive Algo below
 /*
 int fibonacci_naive(int n) {
    // int n=40;
@@ -42,13 +37,16 @@ int fibonacci_naive(int n) {
 
 // need a long long type for this
 
-
+// here is the faster algo below
+// this program calculates the fibonacci sequence 
+// simple by using an array of size n 
 int fibonacci_fast(int n)
 
         {
         
     int  F[n+1];
     int i;
+   // 0 and 1 are the base cases for this program 
             F[0]=0;
             F[1]=1;
     for (int i=2; i<=n; i++)
@@ -66,16 +64,6 @@ int fibonacci_fast(int n)
     //return array[n];
     }
 
-
-// need to include a
-/*
-void test_solution() {
-    assert(fibonacci_fast(3) == 2);
-    assert(fibonacci_fast(10) == 55);
-    for (int n = 0; n < 20; ++n)
-        assert(fibonacci_fast(n) == fibonacci_naive(n));
-}
- */
  
 
 int main()
@@ -85,14 +73,9 @@ int main()
        // cout<< "please enter a number less than 45"<< endl;
     cin >> n;
     
-   //// while (n<=45 )//
-    //{
+   
 
-    cout << fibonacci_fast(n) << '\n';  // remove or commment
-    //test_solution();
-    //std::cout << fibonacci_fast(n) << '\n';
+    cout << fibonacci_fast(n) << '\n';  
     
     }
-    //else
-      ///  cout<< "Please enter an appropriate number less than or equal to 45"
-        //cin>> n;// not sure if i need this ...
+    //...
