3677506d9757ae17b958bf3aebe7bcd694f39c62
palaciosr <36349034+palaciosr@users.noreply.github.com>
2018-03-04 15:53:01 -0800

    Update lcm.cpp

--- lcm/lcm.cpp
+++ lcm/lcm.cpp
@@ -3,26 +3,9 @@ using namespace std;
 // this program is for computing the 
 // least common multiple
 // it makes use of Euclid's algorithmn
+// Here we use recurssion so that the 
+// function calls itself
 
-// gives a naive solution and is very slow for big numbers
-/*long long lcm_naive(int a, int b)
-{
-  for (long l = 1; l <= (long long) a * b; ++l)
-    if (l % a == 0 && l % b == 0)
-      return l;
-
-  return (long long) a * b;
-} */
-
-// try manipulating a and be to x y
-/*
-long long Euclid_gcd_fast(int a, int b )
-{
-    if(a%b==0)
-        return b;
-    else
-        return (long long) Euclid_gcd_fast(b, a%b);
-}  */
 long long Euclid_gcd_fast(int x, int y )
 {
     if(x%y==0)
@@ -30,6 +13,7 @@ long long Euclid_gcd_fast(int x, int y )
 		// hence the mod
         return y;
     else
+	    // callls itself 
         return (long long) Euclid_gcd_fast(y, x%y);
 }
 
@@ -41,6 +25,8 @@ long long lcm_fast(long long a, long long b)
  
     //int n= Euclid_gcd_fast(a,b);
     // uses recurssion 
+    // make a call to the Euclid_gcd_fast  function
+	
     long long z= c/(Euclid_gcd_fast(a,b));
     
     return (long long) z;
